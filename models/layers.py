c8ac921fdd2029ebb14d6522bbe0f196de2a74d4
jhss <mtvfc@naver.com>
2023-01-16 14:13:36 +0000

    feat: Add GateLayer and modified ResNet

---
+++ models/layers.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+class GateLayer(tf.keras.layers.Layer):
+    def __init__(self, input_features, output_features):
+        super(GateLayer, self).__init__()
+        self.weight = self.add_weight(shape = (1, 1, 1, input_features),
+                                      initializer = tf.keras.initializers.Ones())
+        
+    def call(self, x):
+        return tf.multiply(x, self.weight)
+
+
+if __name__ == '__main__':
+    
+    x = tf.random.normal(shape = (2, 3, 3, 4))
+    layer = GateLayer(4, 4)
+    result = layer(x)
